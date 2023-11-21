f85b3771c9fbc92ee3dc37f292d4cb89de1af2ca
RamaKVaitla <150281637+RamaKVaitla@users.noreply.github.com>
2023-11-20 21:55:45 -0500

    Update Jenkins.sh

--- Jenkins.sh
+++ Jenkins.sh
@@ -1,5 +1,4 @@
 amazon-linux-extras install java-openjdk11 -y
-yum install git java-1.8.0-openjdk maven -y
 sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
 yum install jenkins -y
